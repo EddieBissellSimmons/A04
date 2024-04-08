5c1b3d9dcb5cb7093d49d62aafa73d750e48cef8
EddieBissellSimmons <biss2827@mylaurier.ca>
2024-04-08 18:33:06 -0400

    custom element, hopefully..

--- wp-content/themes/a04-rush-theme/customElements.js
+++ wp-content/themes/a04-rush-theme/customElements.js
@@ -1,7 +1,20 @@
 class CopyrightYear extends HTMLElement{
     connectedCallback(){
         this.innerHTML = new Date().getFullYear();
+		
     }
 }
 
 customElements.define("x-year", CopyrightYear);
+
+class EddieElement extends HTMLElement{
+	connectedCallback(){
+		const span = document.createElement: ('span');
+		span.innerHTML = 'c ';
+		const CopyrightYear = new Date().getFullYear();
+		span.innerHTML += '$(CopyrightYear) Eddie Bissell Simmons';
+		this.appendChild(span);
+	}
+}
+
+customElements.define("x-eddie-copyright", EddieElement);
